003dd5250c3f385ad59c88bf578aa653417e0d82
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-12-01 18:37:08 +0000

    Add 9.3.2 software

---
+++ handle-9.3.2/bin/hdl-migrate-storage-to-bdbje.bat
@@ -0,0 +1,2 @@
+@echo off
+%~dp0hdl.bat migrate-storage-to-bdbje %*
---
+++ handle-9.3.2/bin/hdl-server.bat
@@ -0,0 +1,3 @@
+@echo off
+rem Memory configuration can be changed in hdl.bat (search hdl.bat for "memory")
+%~dp0hdl.bat server %*
